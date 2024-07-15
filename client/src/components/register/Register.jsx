f00b147ef50879854b4cf22848bedd702e166a1d
ayoubtahir <ayoub.tahir.2021@gmail.com>
2024-07-15 12:06:07 +0100

    first commit

---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function Register() {
+  const [formData, setFormData] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/data", {
+      credentials: "include", // Include credentials in the request
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-gray-800 text-center">
+          Register
+        </h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-gray-700 mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
